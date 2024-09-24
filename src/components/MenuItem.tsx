dedcf926e9214927501b2ff4dd34e0b790be1b0f
david <dagalisteo@gmail.com>
2024-09-24 19:15:22 +0200

    first commit

---
+++ src/components/MenuItem.tsx
@@ -0,0 +1,13 @@
+import { menuItem } from "../types";
+
+type MenuItemProps = {
+  item: menuItem;
+};
+export default function MenuItem({ item }: MenuItemProps) {
+  return (
+    <button className="border-2 border-teal-400 w-full p-3 flex justify-between hover:bg-teal-200">
+      <p>{item.name}</p>
+      <p className="font-black">${item.price}</p>
+    </button>
+  );
+}
